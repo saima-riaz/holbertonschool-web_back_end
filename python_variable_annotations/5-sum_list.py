1c541ebce900e1ffe1b539dbc354b09f4875dfc5
saima-riaz <saimariaz1422@gmail.com>
2024-01-03 14:15:03 -0800

    function that take list of floats as argument

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Sum_list function takes a list of floats, returns their sum as a float"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of the floats in the input list"""
+    return sum(input_list)
