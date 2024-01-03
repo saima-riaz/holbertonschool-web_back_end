3ff7642046cd49705407dadbfbcbab7a13abdaf8
saima-riaz <saimariaz1422@gmail.com>
2024-01-03 09:35:27 -0800

    function that takes float as argument

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Function 'floor' which takes a float n as an argument
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the float 'n'"""
+    return math.floor(n)
