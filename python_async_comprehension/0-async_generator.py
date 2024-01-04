c59163bfb188af8b553ef8000319a0301c81ae1a
saima-riaz <saimariaz1422@gmail.com>
2024-01-04 14:18:24 -0800

    async_generator that takes no arguments

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Define a coroutine named async_generator that takes no arguments"""
+
+from asyncio import sleep
+from random import uniform
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    """An asynchronous generator that yields random floats"""
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
