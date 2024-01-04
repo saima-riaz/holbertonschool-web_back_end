c342b937daf8882e7cae7d5f9b22f73360342325
saima-riaz <saimariaz1422@gmail.com>
2024-01-04 14:19:55 -0800

    fix error

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ from random import uniform
 from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
-    """An asynchronous generator that yields random floats"""
+    """An asynchronous generator that yields random floats """
     for _ in range(10):
         await sleep(1)
         yield uniform(0, 10)
