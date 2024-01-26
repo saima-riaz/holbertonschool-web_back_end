05e288207cada3c37e83e2116000523c9e279aaa
saima-riaz <saima@Air.lan>
2024-01-26 17:02:42 +0100

    fixed

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,8 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => new Error());
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
