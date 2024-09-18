cd5fb9c608c0cdf5e22821f7829712b23c0ee74a
saima-riaz <saimariaz1422@gmail.com>
2024-09-18 16:40:54 +0200

    print the discard keys

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
+""" FIFO caching system """
 
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
+    """ FIFO caching system that inherits from BaseCaching """
+
     def __init__(self):
+        """ Initialize the class and its attributes """
         super().__init__()
-        self.key_order = []
-    
+        self.key_order = [] # To keep track of insertion order (FIFO)
+
     def put(self, key, item):
-        if key is None and item is None:
+        """ Add an item to the cache following FIFO policy """
+        if key is None or item is None:
             return
+
         if key not in self.cache_data:
-            self.key_order.append(key)
-        
-            self.cache_data[key] = item
-        
+            self.key_order.append(key) # if key is not then add key in the last(track order)
+
+        self.cache_data[key] = item
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key = self.key_order.pop(0)
-            del self.cache_data[first_key]
-            print (f"DISCARD: {first_key}")
-    
+            first_key = self.key_order.pop(0) # Remove the oldest key by using pop
+            del self.cache_data[first_key] # Delete the oldest item
+            print(f"DISCARD: {first_key}") # print the discard keys
+
     def get(self, key):
+        """ Retrieve an item from the cache """
         return self.cache_data.get(key, None)
