6e5bcce220f71bc22e7b3b860f1c5449873236f5
saima-riaz <saimariaz1422@gmail.com>
2024-09-18 16:55:14 +0200

    fix betty

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -10,22 +10,24 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """ Initialize the class and its attributes """
         super().__init__()
-        self.key_order = [] # To keep track of insertion order (FIFO)
+        self.key_order = []  # To track the order of keys (FIFO)
 
     def put(self, key, item):
         """ Add an item to the cache following FIFO policy """
         if key is None or item is None:
             return
 
+        # Add the key to the order list if it is new
         if key not in self.cache_data:
-            self.key_order.append(key) # if key is not then add key in the last(track order)
+            self.key_order.append(key)
 
+        
         self.cache_data[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key = self.key_order.pop(0) # Remove the oldest key by using pop
-            del self.cache_data[first_key] # Delete the oldest item
-            print(f"DISCARD: {first_key}") # print the discard keys
+            first_key = self.key_order.pop(0)  # Remove the oldest key
+            del self.cache_data[first_key]     # Delete the oldest item
+            print(f"DISCARD: {first_key}")    # Print the discarded key
 
     def get(self, key):
         """ Retrieve an item from the cache """
