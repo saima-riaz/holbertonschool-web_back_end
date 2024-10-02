99de36798001960330c9b5155bc48ce8f64e69cf
saima-riaz <saimariaz1422@gmail.com>
2024-10-03 01:00:51 +0200

    error fixed

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -13,20 +13,21 @@ def filter_datum(
         message = re.sub(f'{f}=[^{separator}]*', f'{f}={redaction}', message)
     return message
 
-
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+    """Redacting Formatter class."""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """ Initialize the formatter with fields to obfuscate """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        super().__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Format log record and obfuscate sensitive fields """
-        original_message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
+        """Filters values in incoming log records using filter_datum."""
+        record.msg = filter_datum(
+            self.fields, self.REDACTION,
+            record.getMessage(), self.SEPARATOR
+        )
+        return super().format(record)
