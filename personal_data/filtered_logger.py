0731e8a723da699ab8be974cec58fb6ba3a5e29a
saima-riaz <saimariaz1422@gmail.com>
2024-10-03 00:40:22 +0200

    put comments

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
-""" Regex-ing """
-
-
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    """Returns a log message obfuscated."""
-    for field in fields:
-        message = re.sub(f'{field}=[^{separator}]*', f'{field}={redaction}', message)
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """
+    Replaces sensitive information in a log message with redaction.
+
+    Args:
+        fields: A list of field names to obfuscate.
+        redaction: The string to replace the field values with.
+        message: The log message to be filtered.
+        separator: The character separating fields in the log message.
+    
+    Returns:
+        The filtered log message with obfuscated fields.
+    """
+    for f in fields:
+        message = re.sub(f'{f}=[^ {separator}]*', f'{f}={redaction}', message)
     return message
