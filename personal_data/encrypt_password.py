1e47fe9ab169956d65b87c632347aac190f24b9c
saima-riaz <saimariaz1422@gmail.com>
2024-10-08 17:48:08 +0200

    fixed pycodestyle error

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -4,9 +4,7 @@ Hashes a password using bcrypt."""
 
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """Hashes a password with a new salt"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-
-
-
