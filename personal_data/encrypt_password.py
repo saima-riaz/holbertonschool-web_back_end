670ab168495c10fe19f1cfb2370cd0e38540315a
saima-riaz <saimariaz1422@gmail.com>
2024-10-08 17:46:45 +0200

    fixed code error

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -4,18 +4,9 @@ Hashes a password using bcrypt."""
 
 import bcrypt
 
-
 def hash_password(password: str) -> bytes:
-    # Generate a salt
-    salt = bcrypt.gensalt()
-    # Hash the password
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+    """Hashes a password with a new salt"""
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
-# task 6 
-
-def is_valid(hashed_password: bytes, password:str) -> bool:
-    """check that provided pw matches the hashed pw"""
-    return bcrypt.checkpw(password.encode(), hashed_password)
 
 
