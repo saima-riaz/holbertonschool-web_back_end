d84d56a33d6cb29c0934bfedb16c5919846d6063
saima-riaz <saimariaz1422@gmail.com>
2024-10-08 17:50:35 +0200

    fixed  error

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,3 +8,10 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Hashes a password with a new salt"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+# task 6
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Checks if the provided password matches the hashed password """
+    return bcrypt.checkpw(password.encode(), hashed_password)
