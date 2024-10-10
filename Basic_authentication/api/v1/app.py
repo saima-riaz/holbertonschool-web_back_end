906ae880ca1bd7ec0bb981a15e7dbbb6152a318a
saima-riaz <saimariaz1422@gmail.com>
2024-10-10 17:46:39 +0200

    fixed error handler forbidden

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -8,31 +8,27 @@ from flask import Flask, jsonify, abort
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+# Register blueprint
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
-
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized error handler
-    """
+    """ Unauthorized error handler """
     return jsonify({"error": "Unauthorized"}), 401
-
-
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
-
 
 @app.errorhandler(403)
 def forbidden_error(error):
     """ Custom handler for 403 Forbidden errors """
     return jsonify({"error": "Forbidden"}), 403
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=int(port), debug=True)
