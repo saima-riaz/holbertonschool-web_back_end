41d8c9a693ea520fd3db42552588dca4beabedc6
saima-riaz <saimariaz1422@gmail.com>
2024-10-10 18:25:51 +0200

    create a Auth class and their files

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Authentication module for the API
+"""
+from flask import request
+from typing import List, TypeVar
+
+User = TypeVar('User')
+
+
+class Auth:
+    """ Class to manage the API authentication.
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        if path is None:
+            return True
+
+        if excluded_paths is None:
+            return True
+
+        # Check if the path is in excluded_paths
+        if path in excluded_paths:
+            return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> User:
+        return None
