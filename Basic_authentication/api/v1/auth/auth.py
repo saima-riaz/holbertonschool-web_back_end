c5bfb173eda4a9393a638c98aee0a4c073a7f96d
saima-riaz <saimariaz1422@gmail.com>
2024-10-10 18:40:00 +0200

    error fix

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,19 +8,37 @@ from typing import List, TypeVar
 User = TypeVar('User')
 
 
-class Auth:
+class Auth():
     """ Class to manage the API authentication.
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        if path in excluded_paths:
+        """ Determines if authentication is required for the given path.
+        """
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        normal_path = path if path.endswith('/') else path + '/'
+
+        normal_excluded_paths = [p if p.endswith('/') else p + '/'
+                                 for p in excluded_paths]
+
+        if normal_path in normal_excluded_paths:
             return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
+        """ Retrieves the Authorization header from the request.
+        """
         if request is None or 'Authorization' not in request.headers:
             return None
         return request.headers['Authorization']
 
     def current_user(self, request=None) -> User:
+        """ Retrieves the current user from the request.
+        """
         return None
