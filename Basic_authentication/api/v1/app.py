1e61f0b5e380e0630964ab53bb418975ca398209
saima-riaz <saimariaz1422@gmail.com>
2024-10-10 17:49:38 +0200

    error fixed of pycodestyle

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -13,20 +13,25 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 # Register blueprint
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Unauthorized error handler """
+
     return jsonify({"error": "Unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden_error(error):
     """ Custom handler for 403 Forbidden errors """
     return jsonify({"error": "Forbidden"}), 403
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
