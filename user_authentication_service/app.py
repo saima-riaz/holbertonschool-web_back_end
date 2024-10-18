14ae8feb60df7b590f4c14963ef615867c677922
saima-riaz <saimariaz1422@gmail.com>
2024-10-18 21:15:16 +0200

    plement a login function to respond to the POST /sessions route.

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify, request
+"""App module
+"""
+
+from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
 
 AUTH = Auth()
 
+
 app = Flask(__name__)
-
-
-@app.route("/", methods=["GET"])
-def home():
-    return jsonify(message="Bienvenue")
 
 
 @app.route("/users", methods=["POST"], strict_slashes=False)
@@ -27,5 +26,97 @@ def register_user():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/', methods=["GET"], strict_slashes=False)
+def welcome():
+    """GET route that returns a welcome
+        message in JSON format.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/sessions', methods=["POST"], strict_slashes=False)
+def login():
+    """
+        Endpoint to log in a user.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+
+        session_id = AUTH.create_session(email)
+        response = make_response(jsonify({"email": email,
+                                          "message": "logged in"}))
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """
+        Ennpoint for logout the user
+    """
+    session_id = request.cookies.get('session_id')
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        AUTH.destroy_session(user.id)
+        response = make_response(redirect('/'))
+        response.set_cookie('session_id', '', expires=0)
+        return response
+    else:
+        abort(403)
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """Endpoint to get a user's profile information.
+    """
+    session_id = request.cookies.get('session_id')
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """
+       Endpoint to generate a password reset token for a user.
+    """
+    email = request.form.get('email')
+    if email is None:
+        abort(400)
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    """ Endpoint to update a user's password.
+    """
+    email = request.form.get('email')
+    new_password = request.form.get('new_password')
+    reset_token = request.form.get('reset_token')
+
+    if not all([email, new_password, reset_token]):
+        abort(400)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port="5000")
