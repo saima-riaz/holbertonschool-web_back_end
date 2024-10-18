9f8107be8a26fbf50ea6025091bc95d2c71e1598
saima-riaz <saimariaz1422@gmail.com>
2024-10-18 17:15:11 +0200

    users function that implements the POST /users route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])  # Use "methods" instead of "method"
+@app.route("/", methods=["GET"])
 def home():
     return jsonify(message="Bienvenue")
 
 
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user():
+    """ Endpoint to register a user.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
