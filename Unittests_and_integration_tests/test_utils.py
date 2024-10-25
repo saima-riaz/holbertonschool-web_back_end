c392b4d3ab326c2ea2f9176ac68b9b6398700fab
saima-riaz <saimariaz1422@gmail.com>
2024-10-25 15:58:29 +0200

    Mock HHTP calls

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,6 +28,27 @@ class TestAccessNestedMap(unittest.TestCase):
 
         # Verifying the exception message
         self.assertEqual(str(context.exception), f"'{path[-1]}'")
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')  # Patching 'requests.get' in the 'utils' module
+    def test_get_json(self, test_url, test_payload, mock_get):
+        # Mock the response's .json method to return test_payload
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        # Call get_json with the test_url
+        result = get_json(test_url)
+
+        # Assert that requests.get was called once with test_url
+        mock_get.assert_called_once_with(test_url)
+
+        # Assert the output from get_json is equal to test_payload
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
