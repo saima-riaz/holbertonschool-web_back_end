b6fff64c0a9ed70e936de1de195805381981eba7
saima-riaz <saimariaz1422@gmail.com>
2024-10-25 23:54:05 +0200

    mocking a property, memoize turns methods into proerties

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -22,6 +22,16 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(x.org, expected)
         get_patch.assert_called_once_with("https://api.github.com/orgs/"+org)
 
+    def test_public_repos_url(self):
+        """ Test that _public_repos_url returns the expected result """
+        test_url = "https://api.github.com/orgs/google/repos"
+        payload = {"repos_url": test_url}
+
+        with patch.object(GithubOrgClient, 'org', return_value=payload):
+            client = GithubOrgClient("google")
+            result = client._public_repos_url
+            self.assertEqual(result, test_url)
+
 
 if __name__ == "__main__":
     unittest.main()
