7a6343a11f7c5e69a3494b7180b655e9932d71a5
saima-riaz <saimariaz1422@gmail.com>
2024-10-31 00:27:54 +0100

    setup basis flask app and index.html file

---
+++ i18n/0-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""import module"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+"""define route for root URL"""
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__== '__main__':
+    app.run(host="0.0.0.0", port=5000)
