879480cd8132cc22dae1173627b2376741331c41
saima-riaz <saimariaz1422@gmail.com>
2024-10-31 11:59:47 +0100

    fixed code error

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -4,10 +4,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 """define route for root URL"""
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
