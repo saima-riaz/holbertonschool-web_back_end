5cd70671b0e9398d8d4904aa722d8f941aae062d
saima-riaz <saimariaz1422@gmail.com>
2024-10-31 14:02:10 +0100

    fixed code error

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
-"""import module"""
-from flask import Flask, render_template
-
-app = Flask(__name__)
+""" Route module for the API - Basic Flask app """
 
 
-"""define route for root URL"""
+from flask import Flask, request, render_template
+from os import getenv
+
+app = Flask(__name__, static_url_path='')
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    return render_template('index.html')
+    """ GET /
+        Return: 0-index.html
+    """
+    return render_template('0-index.html')
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
