c90d9f5d673f1357d95fe3c8baf64266c641403e
saima-riaz <saimariaz1422@gmail.com>
2024-11-04 21:58:53 +0100

    add test file

--- 0x0B_redis_basic/test_cache.py
+++ 0x0B_redis_basic/test_cache.py
@@ -1,17 +1,21 @@
-#!/usr/bin/env python3
-"""
-Main file to test the Cache class
-"""
+# test_exercise.py
+
 from exercise import Cache
 
+# Create a Cache instance
 cache = Cache()
 
+# Define test cases
 TEST_CASES = {
     b"foo": None,
     123: int,
     "bar": lambda d: d.decode("utf-8")
 }
 
+# Run tests
 for value, fn in TEST_CASES.items():
     key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
+    # Assert that the retrieved value matches the original input value
+    assert cache.get(key, fn=fn) == value, f"Test failed for value: {value}"
+
+print("All tests passed.")
