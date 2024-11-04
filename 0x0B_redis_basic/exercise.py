45a9484bd7d718c79ec1d8f90de83d4313e9f23b
saima-riaz <saimariaz1422@gmail.com>
2024-11-04 22:08:53 +0100

    fix test file

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
-"""
-cache class to interact with Redis for storing data
-"""
+# exercise.py
+
 import redis
 import uuid
 from typing import Union, Callable, Optional
@@ -9,37 +7,31 @@ from typing import Union, Callable, Optional
 
 class Cache:
     def __init__(self):
-        """initialize Redis client and flush database"""
+        """Initialize Redis client and flush database."""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """generate a random key,store data in Redisusing keys
-        and return key
-        """
+        """Generate a random key,
+        store the data in Redis using the key, and return the key."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
     def get(self, key: str, fn: Optional[Callable] = None
             ) -> Union[str, bytes, int, float, None]:
-        """
-        Retrieve data from Redis by key and optionally apply a conversion
-        function `fn` to the result.
-        """
-        date = self._redis.get(key)
+        """Retrieve data from Redis by key
+        and optionally apply a conversion function `fn`."""
+        data = self._redis.get(key)
         if data is None:
             return None
         return fn(data) if fn else data
 
-    def get_srt(self, key: str) -> Optional[str]:
-        """
-        Retrieve a string from Redis by key, decoding from bytes to str.
-        """
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve a string from Redis by key, decoding from bytes to str."""
         return self.get(key, fn=lambda d: d.decode("utf-8"))
 
     def get_int(self, key: str) -> Optional[int]:
-        """
-        Retrieve an integer from Redis by key, converting from bytes to int.
-        """
+        """Retrieve an integer from Redis by key,
+        converting from bytes to int."""
         return self.get(key, fn=int)
