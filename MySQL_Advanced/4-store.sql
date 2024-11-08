5b2ddc914cc690b9fce02e6a77c75eb11b1ce40a
saima-riaz <saimariaz1422@gmail.com>
2024-11-08 16:54:51 +0100

    fixed

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,5 +1,5 @@
---create a trigger that decreases the quantity of item,
--- after adding new order
+-- Add user privileges - grants `INSERT` privileges to a user on a specific database
+-- Script can be executed on any MySQL server with appropriate privileges
 CREATE TRIGGER order_decrease BEFORE INSERT ON orders
 FOR EACH ROW UPDATE items
 SET quantity = quantity - NEW.number
