ddd2d4dbc1e437ccfa7fa93e366fd0b1835c2a72
saima-riaz <saimariaz1422@gmail.com>
2024-11-13 15:39:58 +0100

    with spies execute the wrapped function, and log useful information

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -2,6 +2,5 @@ import { Utils } from './utils.js';  // Named import
 
 export function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${total}`);
-  console.log(Utils);
+
 }
