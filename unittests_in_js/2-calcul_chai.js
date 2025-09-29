af4a0017b9b8080d96aae9a03f55fad03215dedf
saima-riaz <saimariaz1422@gmail.com>
2025-09-29 13:14:59 +0200

    update

--- unittests_in_js/2-calcul_chai.js
+++ unittests_in_js/2-calcul_chai.js
@@ -1,21 +1,19 @@
-function calculateNumber(type, a, b) {
+ifunction calculateNumber(type, a, b) {
+	const n1 = Math.round(a);
+	const n2 = Math.round(b);
 
-  // Round both numbers
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
+	if (type === 'SUBTRACT') {
+		return n1 - n2;
+	}
 
-  switch(type){
-      case 'sum' :
-          return roundedA + roundedB;
-      case 'sub' :
-          return roundedA - roundedB;
-      case 'division' :
-          if(roundedB === 0) {
-              return "Error";
-          }
-          return roundedA / roundedB;
-      default:
-          throw new Error('Invalid operation type');
-  }
+	if (type === 'DIVIDE') {
+		if (n2 === 0) {
+			return 'Error';
+		}
+		return n1 / n2;
+	}
+
+	return n1 + n2;
 }
+
 module.exports = calculateNumber;
